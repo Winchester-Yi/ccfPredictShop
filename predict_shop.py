3dd7994d01da5a7b855ab27da05a729d1b962dc2
Shi Xiang Yi <619589959@qq.com>
2017-11-20 13:15:42 +0800

    Refactor & Feat：add genetic algorithm
    Add genetic algorithm to train the parameter

--- predict_shop.py
+++ predict_shop.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = 0.9
+__version__ = 1.0
 __author__ = "Yi Shixiang <yishixiang@gmail.com>"
 __date__ = "2017-11-9"
 
@@ -8,6 +8,7 @@ import pandas as pd
 import math
 import numpy as np
 from datetime import datetime
+import random
 
 
 class predict_shop(object):
@@ -33,11 +34,13 @@ class predict_shop(object):
         else:
             print("There is no train file\n")
         self.parameter = parameter
+        self.sampleData = None
         print("Initiation is complete\n")
+        self.strategy = 'top_n'
     pass
 
     # #计算距离
-    def dis(loca1, loca2):
+    def dis(self, loca1, loca2):
         lo1 = loca1[0]
         la1 = loca1[1]
         lo2 = loca2[0]
@@ -54,12 +57,12 @@ class predict_shop(object):
     # #寻找对应商场的店铺
     # shop_info: user_shop_behavior.csv + shop_info.csv
     # mall: 预测商铺所在商场的名称
-    def get_shop(self, *mall):
+    def __get_shop__(self, *mall):
         selected_shop = self.shop_info[self.shop_info['mall_id'].isin(list(mall))]
         return selected_shop
 
     # 返回top-N的dataframe
-    def top_N(self, topn=None, score=None, select_shop=None, shop_id=None):
+    def __top_N__(self, topn=None, score=None, select_shop=None, shop_id=None):
         if shop_id is None:
             index_array = np.argsort(score)
             # #sort self.shop_info by key and select top-N
@@ -86,9 +89,17 @@ class predict_shop(object):
             for single_wifi in user_wifi:
                 locate = np.where(np.isin(shop_wifi, single_wifi[0]))
                 if(locate[0].shape[0] != 0):
-                    count = count + 1
-                    score[i] = score[i] + 100 - abs(float(shop_wifi[locate[0][0]][1]) - float(single_wifi[1]))
-        return (score - score.min()) / (score.ptp()) * 100
+                    if single_wifi[2] == 'true':
+                        count = count + 1
+                        score[i] = score[i] + 4
+                        # score[i] = score[i] + (100 - abs(float(shop_wifi[locate[0][0]][1]) -
+                        #                                  float(single_wifi[1]))) * 1.5
+                    else:
+                        count = count + 1
+                        score[i] = score[i] + 1
+                        # score[i] = score[i] + 100 - abs(float(shop_wifi[locate[0][0]][1]) - float(single_wifi[1]))
+        # return (score - score.min() + 1) / (score.ptp() + 1) * 100
+        return score
 
     # 计算time信息相似度
     # Input
@@ -104,98 +115,239 @@ class predict_shop(object):
             score[i] = np.abs(shop_time - time_info).min()
         return (score - score.min()) / (score.ptp()) * 100
 
-    def get_max_redundancy(self, fraction):
+    def cal_dis(self, select_info, loc):
+        score = np.zeros(select_info.shape[0])
+        for i in range(select_info.shape[0]):
+            score[i] = self.dis((select_info.iloc[i]['longitude'], select_info.iloc[i]['latitude']), loc)
+        score = score.max() - score
+        return (score - score.min()) / (score.ptp()) * 100
+
+    def __get_max_redundancy__(self, train_data):
         # Random select some data to train
-        train_dataset = self.train_info.sample(frac=fraction)
+        train_dataset = train_data
         temp_topn_wifi = []
         temp_topn_time = []
+        temp_topn_dis = []
         for i in range(train_dataset.shape[0]):
-            selected_shop = self.get_shop(self.shop_info[self.shop_info['shop_id'] ==
-                                                         train_dataset.iloc[i]['shop_id']]['mall_id'].iloc[0])
+            selected_shop = self.__get_shop__(self.shop_info[self.shop_info['shop_id'] ==
+                                                             train_dataset.iloc[i]['shop_id']]['mall_id'].iloc[0])
             wifi_score = self.cal_wifi(selected_shop, train_dataset.iloc[i]['wifi_infos'])
             time_info = datetime.strptime(train_dataset.iloc[i]['time_stamp'], '%Y-%m-%d %H:%M')
             time_score = self.cal_time(selected_shop, time_info.hour * 60 + time_info.second)
-            temp_topn_wifi.append(self.top_N(select_shop=selected_shop,
-                                             shop_id=train_dataset.iloc[i]['shop_id'],
-                                             score=wifi_score))
-            temp_topn_time.append(self.top_N(select_shop=selected_shop,
-                                             shop_id=train_dataset.iloc[i]['shop_id'],
-                                             score=time_score))
-            print(i)
+            dis_score = self.cal_dis(selected_shop,
+                                     (train_dataset.iloc[i]['longitude'], train_dataset.iloc[i]['latitude']))
+            temp_topn_wifi.append(self.__top_N__(select_shop=selected_shop,
+                                                 shop_id=train_dataset.iloc[i]['shop_id'],
+                                                 score=wifi_score))
+            temp_topn_time.append(self.__top_N__(select_shop=selected_shop,
+                                                 shop_id=train_dataset.iloc[i]['shop_id'],
+                                                 score=time_score))
+            temp_topn_dis.append(self.__top_N__(select_shop=selected_shop,
+                                                shop_id=train_dataset.iloc[i]['shop_id'],
+                                                score=dis_score))
         temp_topn_wifi = np.array(temp_topn_wifi)
         temp_topn_time = np.array(temp_topn_time)
+        temp_topn_dis = np.array(temp_topn_dis)
         self.parameter['top_n_time'] = temp_topn_time.max()
         self.parameter['top_n_wifi'] = temp_topn_wifi.max()
+        self.parameter['top_n_dis'] = temp_topn_dis.max()
         self.parameter['weight_wifi'] = np.average(temp_topn_wifi)
         self.parameter['weight_time'] = np.average(temp_topn_time)
+        self.parameter['weight_dis'] = np.average(temp_topn_dis)
+        s = np.average(temp_topn_wifi) + np.average(temp_topn_time) + np.average(temp_topn_dis)
+        self.parameter['weight_wifi'] = (s - np.average(temp_topn_wifi)) / (s * 2)
+        self.parameter['weight_time'] = (s - np.average(temp_topn_time)) / (s * 2)
+        self.parameter['weight_dis'] = (s - np.average(temp_topn_dis)) / (s * 2)
+
+        print("Get the max redundancy\n")
         pass
 
-    # Train Parameter
-    # N-Fold train
-    # Parameter:
-    # top_n_wifi top_n_time top_n_dis
-    # weight_wifi weight_tiem weight_dis
-    def train(self, fraction):
-        ##############################################
-        # Optimize top_n_wifi, top_n_time, top_n_dis #
-        ##############################################
-        self.get_max_redundancy()
-        for i in range(self.train_info.shap[0]):
-            selected_shop = self.get_shop(self.train_info.iloc[i]['mall_id'])
-            wifi_score = self.cal_wifi(selected_shop, self.train_info.iloc[i]['wifi_infos'])
-            time_info = datetime.strptime(self.train_info.iloc[i]['time_stamp'], '%Y/%m/%d %H:%M')
-            time_score = self.cal_time(selected_shop, time_info.hour * 60 + time_info.second)
-            self.top_N(selected_shop, 1, wifi_score)
-            self.top_N(selected_shop, 1, time_score)
+    def __predict_shop__(self, selected_shop, data, weight_wifi, weight_time, weight_dis):
+        wifi_score = self.cal_wifi(selected_shop, data['wifi_infos'])
+        time_info = datetime.strptime(data['time_stamp'], '%Y-%m-%d %H:%M')
+        time_score = self.cal_time(selected_shop, time_info.hour * 60 + time_info.second)
+        dis_score = self.cal_dis(selected_shop, (data['longitude'], data['latitude']))
+        total_score = (weight_wifi * wifi_score +
+                       weight_time * time_score +
+                       weight_dis * dis_score)
+        return selected_shop.iloc[total_score.argmax()]['shop_id']
+
+    #########################################
+    #   Grid Search
+    #
+    #########################################
+    def __grid_dots__(self):
+        self.parameter['weight_wifi']
+        self.parameter['weight_time']
+        # self.parameter['weight_dis']
+        grid_wifi = np.arange(self.parameter['weight_wifi'], 1, 0.02)
+        grid_time = np.linspace(self.parameter['weight_time'], 0, grid_wifi.shape[0])
+        grid_dis = np.linspace(self.parameter['weight_dis'], 0, grid_wifi.shape[0])
+        c = np.column_stack((grid_wifi, gri d_time, grid_dis))
+        return c
+
+    def __grid_search__(self, train_dataset):
+        grid = self.__grid_dots__()
+        acc_array = []
+        print("Grid searching... ...\n")
+        for select_dot in grid:
+            j = 0
+            for i in range(train_dataset.shape[0]):
+                selected_shop = self.__get_shop__(self.shop_info[self.shop_info['shop_id'] ==
+                                                                 train_dataset.iloc[i]['shop_id']]['mall_id'].iloc[0])
+                p_shop = self.__predict_shop__(selected_shop, train_dataset.iloc[i],
+                                               select_dot[0], select_dot[1], select_dot[2])
+                if p_shop == train_dataset.iloc[i]['shop_id']:
+                    j = j + 1
+            acc_array.append(j / train_dataset.shape[0])
+        acc_array = np.array(acc_array)
+        index_grid = np.argmax(acc_array)
+        self.parameter['weight_wifi'] = grid[index_grid, 0]
+        self.parameter['weight_time'] = grid[index_grid, 1]
+        print("Grid complete\n")
+
+    #########################################
+    #   Genetic Algorithm
+    #
+    #########################################
+    def __variation__(self, next_generation, varitation_ratio):
+        x = np.random.random(next_generation.shape[1])
+        for i in range(x.shape[0]):
+            if x[i] < varitation_ratio:
+                next_generation[i][0] = next_generation[i][0] + random.uniform(-0.3, 0.3)
+                next_generation[i][1] = next_generation[i][1] + random.uniform(-0.3, 0.3)
+                next_generation[i][2] = next_generation[i][2] + random.uniform(-0.3, 0.3)
+        return next_generation
         pass
 
-    # 其余特征
-    # evaluate_info: 测试集
-    # shop_info: shop_id, category, longitude, latitude, price, mall_id, time_stamp, wifi_infos
-    def predict(self):
-        predict_shop = []
-        for i in range(self.predict_info.shape[0]):
-            selected_shop = self.get_shop(self.predict_info.iloc[i]['mall_id'])
-            wifi_score = self.cal_wifi(selected_shop, self.predict_info.iloc[i]['wifi_infos'])
-            time_info = datetime.strptime(self.predict_info.iloc[i]['time_stamp'], '%Y-%m-%d %H:%M')
-            time_score = self.cal_time(selected_shop, time_info.hour * 60 + time_info.second)
-            total_score = 0.71 * wifi_score + 0.29 * time_score
-            predict_shop.append(selected_shop.iloc[total_score.argmax()]['shop_id'])
-            print(i)
-        result = pd.DataFrame({'row_id': self.predict_info.row_id, 'shop_id': predict_shop})
-        result.fillna('s_666').to_csv('first.csv', index=None)
+    def __cross__(self, next_generation, cross_ratio):
+        x = np.random.random(next_generation.shape[1])
+        for i in range(x.shape[0]):
+            if x[i] < cross_ratio:
+                next_generation[i][0] = next_generation[i][0] + next_generation[i + 1][0] * random.uniform(-0.5, 0.5)
+                next_generation[i][1] = next_generation[i][1] + next_generation[i + 1][1] * random.uniform(-0.5, 0.5)
+                next_generation[i][2] = next_generation[i][2] + next_generation[i + 1][2] * random.uniform(-0.5, 0.5)
+        return next_generation
+
+    def __wheel_selection__(self, genetic, fitness):
+        s = fitness / fitness.sum()
+        s = np.cumsum(s)
+        result = np.zeros((genetic.shape[0], 3))
+        select_proba = np.random.random(s.shape[0])
+        for i in range(s.shape[0]):
+            x = np.where(s > select_proba[i])[0].min()
+            result[i] = genetic[x]
+        return result
+
+    def __fitness__(self, genetic, train_dataset):
+        j = 0
+        fit = np.zeros(genetic.shape[0])
+        for x in range(genetic.shape[0]):
+            for i in range(train_dataset.shape[0]):
+                selected_shop = self.__get_shop__(self.shop_info[self.shop_info['shop_id'] ==
+                                                                 train_dataset.iloc[i]['shop_id']]['mall_id'].iloc[0])
+                wifi_score = self.cal_wifi(selected_shop, train_dataset.iloc[i]['wifi_infos'])
+                time_info = datetime.strptime(train_dataset.iloc[i]['time_stamp'], '%Y-%m-%d %H:%M')
+                time_score = self.cal_time(selected_shop, time_info.hour * 60 + time_info.second)
+                dis_score = self.cal_dis(selected_shop, (train_dataset.iloc[i]['longitude'], train_dataset.iloc[i]['latitude']))
+                total_score = (genetic[x][0] * wifi_score +
+                               genetic[x][1] * time_score +
+                               genetic[x][2] * dis_score)
+                if selected_shop.iloc[total_score.argmax()]['shop_id'] == train_dataset.iloc[i]['shop_id']:
+                    j += 15
+                else:
+                    j += random.randint(1, 4)
+            # if j != 0:
+            #     total_score = 100
+            # else:
+            #     total_score = random.randint(10, 70)
+            fit[x] = j
+        return fit
+
+    def __genetic_search__(self, train_dataset, pro_var, pro_cross):
+        ###############
+        # Initiate seed
+        ###############
+        seed_wifi = np.random.triangular(self.parameter['weight_wifi'], (1 + self.parameter['weight_wifi']) / 2, 1, 20)
+        seed_time = np.random.triangular(0, self.parameter['weight_time'] / 2, self.parameter['weight_time'], 20)
+        seed_dis = np.random.triangular(0, self.parameter['weight_dis'] / 2, self.parameter['weight_dis'], 20)
+        seed = np.column_stack((seed_wifi, seed_time, seed_dis))
+        fit = np.array(self.__fitness__(seed, train_dataset))
+        print(seed.shape)
+        for times in range(20):
+            print("Time:", times)
+            next_generation = self.__wheel_selection__(seed, fit)
+            print(next_generation)
+            next_generation = self.__cross__(next_generation, pro_cross)
+            next_generation = self.__variation__(next_generation, pro_var)
+            fit = np.array(self.__fitness__(next_generation, train_dataset))
+        self.parameter['weight_wifi'] = next_generation[0][np.argmax(fit)]
+        self.parameter['weight_time'] = next_generation[1][np.argmax(fit)]
+        self.parameter['weight_dis'] = next_generation[2][np.argmax(fit)]
+        print("Max fitness:\t", fit.max())
 
     def test(self, fraction):
         test_dataset = self.train_info.sample(frac=fraction)
         j = 0
         for i in range(test_dataset.shape[0]):
-            selected_shop = self.get_shop(self.shop_info[self.shop_info['shop_id'] ==
-                                                         test_dataset.iloc[i]['shop_id']]['mall_id'].iloc[0])
+            selected_shop = self.__get_shop__(self.shop_info[self.shop_info['shop_id'] ==
+                                                             test_dataset.iloc[i]['shop_id']]['mall_id'].iloc[0])
             wifi_score = self.cal_wifi(selected_shop, test_dataset.iloc[i]['wifi_infos'])
             time_info = datetime.strptime(test_dataset.iloc[i]['time_stamp'], '%Y-%m-%d %H:%M')
             time_score = self.cal_time(selected_shop, time_info.hour * 60 + time_info.second)
-            total_score = 0.71 * wifi_score + 0.29 * time_score
+            dis_score = self.cal_dis(selected_shop, (test_dataset.iloc[i]['longitude'],
+                                                     test_dataset.iloc[i]['latitude']))
+            total_score = (self.parameter['weight_wifi'] * wifi_score +
+                           self.parameter['weight_time'] * time_score +
+                           self.parameter['weight_dis'] * dis_score)
             print(i)
             if selected_shop.iloc[total_score.argmax()]['shop_id'] == test_dataset.iloc[i]['shop_id']:
                 j = j + 1
         print("ACC:", j / test_dataset.shape[0])
+
+    def train(self, fraction, strategy='grid_search'):
+        ##############################################
+        # Optimize top_n_wifi, top_n_time, top_n_dis #
+        ##############################################
+        if strategy == 'grid_search':
+            self.strategy = 'grid_search'
+            train_dataset = self.train_info.sample(frac=fraction)
+            self.__get_max_redundancy__(train_data=train_dataset)
+            print("Parameter Seed:\n", self.parameter)
+            self.__grid_search__(train_dataset=train_dataset)
+            print("Trained parameter:\n", self.parameter)
+        elif strategy == "genetic":
+            pro_var = 0.06
+            pro_cross = 0.6
+            train_dataset = self.train_info.sample(frac=fraction)
+            self.__get_max_redundancy__(train_data=train_dataset)
+            print("Parameter Seed:\n", self.parameter)
+            self.__genetic_search__(train_dataset, pro_var, pro_cross)
+            print("Trained parameter:\n", self.parameter)
+            pass
+        else:
+            pass
         pass
 
-    # # 发生行为时最大距离
-    # loca1 = info.as_matrix(['longitude1', 'latitude1'])
-    # loca2 = info.as_matrix(['longitude2', 'latitude2'])
-    # max = 0
-    # for (x, y, i) in zip(loca1, loca2, range(loca1.shape[0])):
-    #     distence = dis(x, y)
-    #     if max <= distence:
-    #         max = distence
-    #     if i % 100 == 0:
-    #         print("%d times" % i)
-    #         print(max)
-    # print(max)
-    # print("max1 = 0.24418311746329538")
-    # print("max2 = 0.016919575866857273")
-    # print("max3 = 0.2885169098608471")
+    def predict(self):
+        predict_shop = []
+        print("Predicting... ...")
+        # for i in range(self.predict_info.shape[0]):
+        for i in range(320000, 400000):
+            selected_shop = self.__get_shop__(self.predict_info.iloc[i]['mall_id'])
+            wifi_score = self.cal_wifi(selected_shop, self.predict_info.iloc[i]['wifi_infos'])
+            time_info = datetime.strptime(self.predict_info.iloc[i]['time_stamp'], '%Y-%m-%d %H:%M')
+            time_score = self.cal_time(selected_shop, time_info.hour * 60 + time_info.second)
+            dis_score = self.cal_dis(selected_shop, (self.predict_info.iloc[i]['longitude'],
+                                                     self.predict_info.iloc[i]['latitude']))
+            total_score = (self.parameter['weight_wifi'] * wifi_score +
+                           self.parameter['weight_time'] * time_score +
+                           self.parameter['weight_dis'] * dis_score)
+            predict_shop.append(selected_shop.iloc[total_score.argmax()]['shop_id'])
+            print(i)
+        result = pd.DataFrame({'row_id': self.predict_info.row_id, 'shop_id': predict_shop})
+        result.fillna('s_666').to_csv('first.csv', index=None)
+        print("Done!")
 
 
 if __name__ == '__main__':
@@ -204,5 +356,7 @@ if __name__ == '__main__':
                            train_info='train-ccf_first_round_user_shop_behavior.csv'
                            )
 
-    example.predict()
+    example.train(fraction=0.001, strategy="genetic")
+    example.test(fraction=0.001)
+    # example.predict()
     pass
